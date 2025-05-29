486393816ba00c24419071a11b9266d2e0205163
Francirleide <francirleide.trajano@escolar.ifrn.edu.br>
2025-05-29 13:03:48 +0000

    aula 23/05

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import "./App.css";
-import "./styles/global.css";
 import "./styles/theme.css";
-
+import "./styles/global.css";
+import { MyText } from "./components/MyText";
 
 export default function App() {
+  const texts = [
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+  ];
+
   return (
+    // React Fragment
     <>
-     <Mytext/>
-     <Mytext/> 
-     <Mytext/>
+      {texts.map((text, index) => {
+        return (
+          <MyText key={index} title={`${text.title} ${index + 1}`}>
+            {text.text}
+          </MyText>
+        );
+      })}
     </>
   );
 }
